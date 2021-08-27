1d5aaa73e5631e39b2d09b3b780b1151c7ec9576
pranav1699 <pranavsadagopan@gmail.com>
2021-08-27 22:42:23 +0530

    'done'

---
+++ pages/coin/[id].js
@@ -0,0 +1,32 @@
+import Layout from "../../components/Layout";
+import styles from "./Coin.module.css";
+
+function Coin({ coin }) {
+    return (
+        <Layout>
+            <div className={styles.coin_page}>
+                <div className={styles.coin_container}>
+                    <img className={styles.coin_image} src={coin.image.large} alt={coin.name} />
+                    <h1 className={styles.coin_name}>{coin.name}</h1>
+                    <p className={styles.coin_ticker}>{coin.symbol}</p>
+                    <p className={styles.coin_current}>${coin.market_data.current_price.usd} </p>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default Coin;
+
+export async function getServerSideProps(context){
+    const {id} = context.query
+    const res = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`)
+
+    const data = await res.json()
+
+    return {
+        props:{
+            coin:data
+        }
+    }
+}
